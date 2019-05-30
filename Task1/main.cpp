760598cbab55abbde659bf0c7ac9ee71caeaf8c8
Alexander Goncharuk <sashael@ukr.net>
2019-05-30 16:25:47 +0300

    ttt

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main()
 {
 	srand(time(nullptr));
-	Student* group[2];
-	group[1] = new Student{InitializeRandomly()};
-	group[1]->ShowFullInfo();
-	group[0]->ShowFullInfo();
-	//test
+	Student** group = new Student * [2];
+	group[1] = new Student[10];
+	//group[1] = new Student{InitializeRandomly()};
+	//group[1]->ShowFullInfo();
+	group[0][1].ShowFullInfo();
 	return 0;
 }
